8d1ccf6483a1aa1ccb2a27f32df0a643e5bc014b
Sr-Lechuga <jonattanlima@gmail.com>
2025-09-23 02:54:01 -0300

    Refactor: Update activity instance routes to include request validation schema and todo endpoints.

--- Backend/src/1_routes/activityInsctance.route.js
+++ Backend/src/1_routes/activityInsctance.route.js
@@ -2,12 +2,26 @@ const express = require("express");
 const {
   getActivityInstances,
 } = require("../3_controllers/activityInstance.controller");
+const {
+  activityInstancePostRequestSchema,
+} = require("../2_middlewares/request_schemas/activityInstanceRequest.schema");
 
 const activityInstanceRoute = express.Router();
 
-//activityInstanceRoute.post("/",  payloadValidator(volunteerLoginRequestSchema),  postActivityInstances);
-//activityInstanceRoute.put("/", getActivityInstances);
+// activityInstanceRoute.post(
+//   "/",
+//   payloadValidator(activityInstancePostRequestSchema),
+//   postActivityInstances
+// );
+
+// activityInstanceRoute.put(
+//   "/",
+//   payloadValidator(activityInstancePostRequestSchema),
+//   putActivityInstances
+// );
+
 activityInstanceRoute.get("/", getActivityInstances);
-//activityInstanceRoute.delete("/", getActivityInstances);
+
+//activityInstanceRoute.delete("/", deleteInstancePostRequestSchema);
 
 module.exports = activityInstanceRoute;
