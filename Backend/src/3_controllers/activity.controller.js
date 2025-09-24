b55369b07c5be92e8103af6c059081599ae2cd91
Sr-Lechuga <jonattanlima@gmail.com>
2025-09-23 21:03:42 -0300

    fix: Update getActivities function to handle empty results and improve error handling

--- Backend/src/3_controllers/activity.controller.js
+++ Backend/src/3_controllers/activity.controller.js
@@ -1,16 +1,17 @@
-const { activitiesSelect } = require("../4_services/activity.service")
+const { activitiesSelect } = require("../4_services/activity.service");
 
 const getActivities = async (req, res) => {
   try {
-    const activities = await activitiesSelect(req.query)
-    res.status(200).json({ activities })
+    const activities = await activitiesSelect(req.query);
+    if (activities.length == 0)
+      res.status(204).json({ messages: "No hay actividades para mostrar" });
+    res.status(200).json({ activities });
+  } catch (err) {
+    err.placeOfError = "Error en obtener actividades";
+    next(err);
   }
-  catch (err) {
-    err.placeOfError = 'Error en obtener actividades'
-    next(err)
-  }
-}
+};
 
 module.exports = {
-  getActivities
-}
+  getActivities,
+};
