312a1cc2e8da860358e2c99bd65effe20013eeb4
Sr-Lechuga <jonattanlima@gmail.com>
2025-09-28 18:04:31 -0300

    refactor: Enhance validation schemas for volunteer and organizer requests, improving error messages and structure; update controller logic for better clarity and consistency

--- Backend/src/3_controllers/auth.controller.js
+++ Backend/src/3_controllers/auth.controller.js
@@ -1,87 +1,123 @@
-const { organizerLogin, organizerSignUp } = require("../4_services/organizer.service")
-const { volunteerLogin, volunteerSignUp } = require("../4_services/volunteer.service")
+const {
+  organizerLogin,
+  organizerSignUp,
+} = require("../4_services/organizer.service");
+const {
+  volunteerLogin,
+  volunteerSignUp,
+} = require("../4_services/volunteer.service");
 
 // VOLUNTEERS
 // Nota: Elegimos mantener toda la lógica entre voluntarios y organizadores por separado a pesar de que se repita código
 // La intención de la decisión es que si a futuro algo varía entre volunteers y organizers, los cambios sean más sencillos de implementar
 const postVolunteerLogin = async (req, res, next) => {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   try {
-    const sesionToken = await volunteerLogin(username, password)
+    const sesionToken = await volunteerLogin(username, password);
 
     if (!sesionToken) {
-      res.status(400).json({ message: 'Credenciales incorrectas' })
-      return
+      res.status(400).json({ message: "Credenciales incorrectas" });
+      return;
     }
 
     res.status(200).json({
-      message: 'Inicio de sesión correcta',
-      token: sesionToken
-    })
+      message: "Inicio de sesión correcta",
+      token: sesionToken,
+    });
+  } catch (err) {
+    err.placeOfError = "Inicio de sesión de Volunteer";
+    next(err);
   }
-  catch (err) {
-    err.placeOfError = 'Inicio de sesión de Volunteer'
-    next(err)
-  }
-}
+};
 
 const postVolunteerSignUp = async (req, res, next) => {
-  const { username, email, password, name, lastname, age, genre } = req.body
+  const { username, email, password, name, lastname, age, genre } = req.body;
   try {
     // Se hace de esta forma y no directamente del req.body para que quede mas claro que atributos van al nuevo voluntario
-    const newVolunteer = { username, email, password, name, lastname, age, genre }
-    await volunteerSignUp(newVolunteer)
+    const newVolunteer = {
+      username,
+      email,
+      password,
+      name,
+      lastname,
+      age,
+      genre,
+      registrationDate: Date.now(),
+    };
+    await volunteerSignUp(newVolunteer);
 
-    res.status(201).json({ message: 'Voluntario registrado correctamente. Puede iniciar sesión' })
+    res
+      .status(201)
+      .json({
+        message: "Voluntario registrado correctamente. Puede iniciar sesión",
+      });
+  } catch (err) {
+    err.placeOfError = "Registro [SignUp] de Volunteer";
+    next(err);
   }
-  catch (err) {
-    err.placeOfError = 'Registro [SignUp] de Volunteer'
-    next(err)
-  }
-}
+};
 // END VOLUNTEERS
-
 
 // ORGANIZERS
 const postOrganizerLogin = async (req, res, next) => {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   try {
-    const sesionToken = await organizerLogin(username, password)
+    const sesionToken = await organizerLogin(username, password);
 
     if (!sesionToken) {
-      res.status(400).json({ message: 'Credenciales incorrectas' })
-      return
+      res.status(400).json({ message: "Credenciales incorrectas" });
+      return;
     }
 
     res.status(200).json({
-      message: 'Inicio de sesión correcta',
-      token: sesionToken
-    })
+      message: "Inicio de sesión correcta",
+      token: sesionToken,
+    });
+  } catch (err) {
+    err.placeOfError = "Inicio de sesión de Organizer";
+    next(err);
   }
-  catch (err) {
-    err.placeOfError = 'Inicio de sesión de Organizer'
-    next(err)
-  }
-}
+};
 
 const postOrganizerSignUp = async (req, res, next) => {
-  const { username, email, contactEmail, password, name, telephone, social, location } = req.body
+  const {
+    username,
+    email,
+    contactEmail,
+    password,
+    name,
+    telephone,
+    social,
+    location,
+  } = req.body;
   try {
-    const newOrganizer = { username, email, contactEmail, password, name, telephone, social, location }
-    await organizerSignUp(newOrganizer)
+    const newOrganizer = {
+      username,
+      email,
+      contactEmail,
+      password,
+      name,
+      telephone,
+      social,
+      location,
+    };
+    await organizerSignUp(newOrganizer);
 
-    res.status(201).json({ message: 'Organizador registrado correctamente. Puede iniciar sesión' })
+    res
+      .status(201)
+      .json({
+        message: "Organizador registrado correctamente. Puede iniciar sesión",
+      });
+  } catch (err) {
+    err.placeOfError = "Registro [SignUp] de Organizer";
+    next(err);
   }
-  catch (err) {
-    err.placeOfError = 'Registro [SignUp] de Organizer'
-    next(err)
-  }
-}
+};
 // END ORGANIZERS
 
 module.exports = {
   postVolunteerLogin,
   postVolunteerSignUp,
   postOrganizerLogin,
-  postOrganizerSignUp
-}
+  postOrganizerSignUp,
+};
--- Backend/src/models/volunteer.model.js
+++ Backend/src/models/volunteer.model.js
@@ -1,28 +1,41 @@
-const mongoose = require('mongoose')
-const { VOLUNTEER_GENRE } = require('../utils/constants')
+const mongoose = require("mongoose");
+const { VOLUNTEER_GENRE } = require("../utils/constants");
 
 const volunteerSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true, minLength: 3, maxLength: 30 },
-    email: { type: String, required: true, unique: true, match: /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/, maxLength: 70 },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/,
+      maxLength: 70,
+    },
     password: { type: String, required: true, minLength: 6 },
     name: { type: String, required: true, minLength: 3, maxLength: 30 },
     lastname: { type: String, required: true, minLength: 3, maxLength: 30 },
     age: { type: Number, requird: true, min: 18, max: 110 },
-    genre: { type: String, enum: [...VOLUNTEER_GENRE] }
+    genre: { type: String, enum: [...VOLUNTEER_GENRE] },
+    registrationDate: { type: Date, required: true, default: Date.now },
   },
   {
     timestamps: true,
-    collection: 'volunteers',
+    collection: "volunteers",
     toJSON: {
       transform: function (doc, ret) {
-        delete ret.__v
-        delete ret.password
-      }
-    }
+        delete ret.__v;
+        delete ret.password;
+      },
+    },
   }
-)
+);
 
-const Volunteer = mongoose.model('Volunteer', volunteerSchema)
+const Volunteer = mongoose.model("Volunteer", volunteerSchema);
 
-module.exports = Volunteer
+module.exports = Volunteer;
