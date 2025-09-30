14905ee42c0f36a5aecc30f2b252c4faa2b9952f
Sr-Lechuga <jonattanlima@gmail.com>
2025-09-29 22:45:23 -0300

    fix: Update token handling in verifySesion middleware to remove "Bearer " prefix and improve error response formatting

--- Backend/src/2_middlewares/verifySesion.middleware.js
+++ Backend/src/2_middlewares/verifySesion.middleware.js
@@ -1,16 +1,19 @@
-const { verifyToken } = require("../4_services/helpers/jwt.helper")
+const { verifyToken } = require("../4_services/helpers/jwt.helper");
 
 const verifySesion = async (req, res, next) => {
-  const token = req.headers['authorization']
-  const decodedUserData = verifyToken(token)
+  // In case the token comes with the "Bearer " prefix, we need to remove it
+  const token = req.headers["authorization"]?.replace("Bearer ", "");
+  const decodedUserData = verifyToken(token);
 
   if (!token || !decodedUserData) {
-    res.send(401).json({ message: 'Sesión expirada o inválida. Por favor inicie sesión nuevamente' })
-    return
+    res.send(401).json({
+      message: "Sesión expirada o inválida. Por favor inicie sesión nuevamente",
+    });
+    return;
   }
 
-  req.session = decodedUserData
-  next()
-}
+  req.session = decodedUserData;
+  next();
+};
 
-module.exports = verifySesion
+module.exports = verifySesion;
