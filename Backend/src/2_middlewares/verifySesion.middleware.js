4db1d5f31879ebb4aa38a6e841c50fbb93697e10
Sr-Lechuga <jonattanlima@gmail.com>
2025-09-29 23:36:43 -0300

    fix: Improve error handling in verifySesion middleware for better readability

--- Backend/src/2_middlewares/verifySesion.middleware.js
+++ Backend/src/2_middlewares/verifySesion.middleware.js
@@ -1,16 +1,18 @@
-const { verifyToken } = require("../4_services/helpers/jwt.helper")
+const { verifyToken } = require("../4_services/helpers/jwt.helper");
 
 const verifySesion = async (req, res, next) => {
-  const token = req.headers['authorization']
-  const decodedUserData = verifyToken(token)
+  const token = req.headers["authorization"];
+  const decodedUserData = verifyToken(token);
 
   if (!token || !decodedUserData) {
-    res.send(401).json({ message: 'Sesión expirada o inválida. Por favor inicie sesión nuevamente' })
-    return
+    res.status(401).json({
+      message: "Sesión expirada o inválida. Por favor inicie sesión nuevamente",
+    });
+    return;
   }
 
-  req.session = decodedUserData
-  next()
-}
+  req.session = decodedUserData;
+  next();
+};
 
-module.exports = verifySesion
+module.exports = verifySesion;
