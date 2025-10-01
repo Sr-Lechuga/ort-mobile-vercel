a87b870b90b56bf6abeb3b3af31d005e926dcb5b
Sr-Lechuga <jonattanlima@gmail.com>
2025-10-01 01:57:31 -0300

    feat: Integrate rate limiting middleware to enhance request handling and prevent abuse

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const rateLimiter = require("./src/config/rateLimiter");
 const connectDB = require("./src/config/mongodb");
 const errorHandler = require("./src/2_middlewares/errorHandler.middleware");
 const Volunteer = require("./src/models/volunteer.model");
@@ -16,6 +17,7 @@ connectDB();
 const app = express();
 
 // Middlewares
+app.use(rateLimiter);
 app.use(express.json());
 app.use(morgan("dev"));
 
